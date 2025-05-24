959bb498aeb26dafa9fa0cf258c8ebd869536c65
DayDream <huangyaodong75@gmail.com>
2025-05-24 11:58:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8001",
+        target: "http://0.0.0.0:8001",
+        allowedHosts: 'all',
         changeOrigin: true,
         rewrite: (path) => path,
         configure: (proxy, options) => {
